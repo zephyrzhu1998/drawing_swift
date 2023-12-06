fca1458b78cb971ad129d424acb061e7bcc07109
zephyr <zephyrzhu1998@gmail.com>
2023-12-06 16:51:28 +0800

    init

---
+++ draw_app/draw_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  draw_app
+//
+//  Created by User1 Miko on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ draw_app/draw_app/draw_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  draw_appApp.swift
+//  draw_app
+//
+//  Created by User1 Miko on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct draw_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
